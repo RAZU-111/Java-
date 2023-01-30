a651a4bcf25945e80ac4604b8c4534e63d53bdfe
razu <razujcs@gmail.com>
2023-01-31 00:35:50 +0600

    first commit

---
+++ Loops.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Loops {
+    // int number;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        for (int i = 0; i < number ; i++) {
+           System.out.println("Hello ");
+        }
+    }
+}
